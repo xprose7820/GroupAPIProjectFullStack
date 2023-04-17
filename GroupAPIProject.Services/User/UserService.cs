e245d026b4c83a0966d2eddbe744654eeb9cfa9a
NAHowell <nahowell2321@hotmail.com>
2023-04-17 17:05:07 -0400

    User/Customer

---
+++ GroupAPIProject.Services/User/UserService.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GroupAPIProject.Data;
+using GroupAPIProject.Data.Entities;
+using GroupAPIProject.Models.User;
+
+namespace GroupAPIProject.Services.User
+{
+    public class UserService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UserService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<bool> RegisterUserAsync(UserRegister model)
+        {
+            if (await GetUserByUsername(model.Username) != null)
+            {
+                return false;
+            }
+            UserEntity entity = new UserEntity
+            {
+                Username = model.Username
+            };
+            PasswordHasher<UserEntity> passwordHasher = new PasswordHasher<UserEntity>();
+            entity.Password = passwordHasher.HashPassword(entity, model.Password);
+            _context.Users.Add(entity);
+            int numberOfChanges = await _context.SaveChangesAsync();
+            return numberOfChanges == 1;
+        }
+        public async Task<UserEntity> GetUserByUsername(string username)
+        {
+            return await _context.Users.FirstOrDefaultAsync(user => user.Username.ToLower() == username.ToLower());
+        }
+    }
+}
--- GroupAPIProject.WebAPI/Controllers/UserController.cs
+++ GroupAPIProject.WebAPI/Controllers/UserController.cs
@@ -2,11 +2,22 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using GroupAPIProject.Services.Token;
+using GroupAPIProject.Services.User;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
 
 namespace GroupAPIProject.WebAPI.Controllers
 {
+    [ApiController]
     public class UserController
     {
-        
+        private readonly IUserService _userService; 
+        private readonly ITokenService _tokenService;
+
+        public UserController(IUserService userService, ITokenService tokenService){
+            _userService = userService;
+            _tokenService = tokenService;
+        }   
     }
 }
