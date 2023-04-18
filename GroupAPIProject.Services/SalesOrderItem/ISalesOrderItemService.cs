31a58748ea1be1365e86b6835f0ee0864052d9e2
Alex <xprose7820@gmail.com>
2023-04-18 11:30:15 -0400

    updated create method

--- GroupAPIProject.Services/SalesOrderItem/ISalesOrderItemService.cs
+++ GroupAPIProject.Services/SalesOrderItem/ISalesOrderItemService.cs
@@ -2,11 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using GroupAPIProject.Models.SalesOrderItem;
 
 namespace GroupAPIProject.Services.SalesOrderItem
 {
     public interface ISalesOrderItemService
     {
-        
+        Task<bool> CreateSalesOrderItemAsync(SalesOrderItemCreate model);
     }
 }
